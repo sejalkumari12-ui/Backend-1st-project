15b9e2be9823e418b1d65849da0469f33c75e5e5
sejal <user.email: sejalkumari5679@gmail.com>
2025-07-18 14:46:07 +0530

    error solve

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n✅ MongoDB connected! DB Host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("❌ MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
 
-const connectDB = async()=>{
-    try {
-        const connectionIntsance=await mongoose.connect(`${process.env.mongoose}/${DB_NAME}`)
-        console.log(`\n Mongoose connected !! DB Host:${connectionIntsance.connection.host}`)
-        
-    } catch (error) {
-        console.log("Mongose connection error",error)
-        process.exit(1)
-        
-    }
-}
 export default connectDB;
+
+ 
